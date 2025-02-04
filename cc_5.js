06ffffeed41f6ca3d8bd9b3845d33d7eae451179
HIRUN <hirundesilva97@gmail.com>
2025-02-04 09:45:00 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1 - Customer Profile           
+let customer = {            //declared object customer with properties
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@gmail.com"
+} 
+console.log("Task 1")       //logged each property with console.log and template literal
+console.log(`   Customer Name: ${customer.name}`)
+console.log(`   Customer Age: ${customer.age}`)
+console.log(`   Customer Email: ${customer.email}`)
+
